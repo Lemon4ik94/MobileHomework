fb347cd82e19a51c976da6eb53591d50b7fe8c95
Lemon4ik94 <olegborbych@gmail.com>
2024-02-10 12:45:15 +0200

    Added avatar

--- app/src/main/java/com/example/testwork/MainActivity.kt
+++ app/src/main/java/com/example/testwork/MainActivity.kt
@@ -3,14 +3,31 @@ package com.example.testwork
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CardDefaults.shape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.testwork.ui.theme.TestWorkTheme
+import com.google.ai.client.generativeai.type.content
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,9 +37,9 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color(0xFFEDEFE3)
                 ) {
-                    Greeting("Android")
+                    Settings()
                 }
             }
         }
@@ -30,17 +47,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Settings(modifier: Modifier = Modifier) {
+
+    Column {
+        Spacer(modifier.height(27.dp))
+        Row(Modifier.fillMaxWidth(1f), horizontalArrangement = Arrangement.End) {
+            Image(
+                painter = painterResource(id = R.drawable.avatar),
+                contentDescription = "",
+                modifier = modifier
+                    .clip(RoundedCornerShape(50))
+                    .width(51.dp)
+                    .height(51.dp)
+            )
+            Spacer(modifier = modifier.width(22.dp))
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun GreetingPreview() {
-    TestWorkTheme {
-        Greeting("Android")
+fun SettingsPreview() {
+    TestWorkTheme() {
+        Settings()
     }
 }
