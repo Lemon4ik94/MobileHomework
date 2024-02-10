efa970eaf6c14cdf8169b65c8dc373a6e3708f58
Lemon4ik94 <olegborbych@gmail.com>
2024-02-10 13:58:19 +0200

    Almost ready

--- app/src/main/java/com/example/testwork/MainActivity.kt
+++ app/src/main/java/com/example/testwork/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.testwork
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,22 +14,26 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.CardDefaults.shape
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.testwork.ui.theme.TestWorkTheme
-import com.google.ai.client.generativeai.type.content
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,7 +55,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Settings(modifier: Modifier = Modifier) {
 
-    Column {
+    Column(modifier.padding(horizontal = 23.dp)) {
         Spacer(modifier.height(27.dp))
         Row(Modifier.fillMaxWidth(1f), horizontalArrangement = Arrangement.End) {
             Image(
@@ -60,7 +66,96 @@ fun Settings(modifier: Modifier = Modifier) {
                     .width(51.dp)
                     .height(51.dp)
             )
-            Spacer(modifier = modifier.width(22.dp))
+        }
+        Spacer(modifier.height(15.dp))
+        Text(text = "Settings", fontSize = 36.sp, fontWeight = FontWeight(400))
+        Spacer(modifier.height(23.dp))
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .clip(
+                    RoundedCornerShape(25.dp)
+                )
+                .background(color = Color(0xFFDBEA8D))
+                .padding(horizontal = 16.dp, vertical = 22.dp)
+        ) {
+            Text(text = "icon")
+            Spacer(modifier.width(16.dp))
+            Column {
+                Text(
+                    text = "Get to know your Pixel",
+                    fontSize = 21.sp,
+                    fontWeight = FontWeight(500),
+                    lineHeight = 25.03.sp
+                )
+                Spacer(modifier.height(5.dp))
+                Text(
+                    text = "Explore what you can do with your phone",
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight(400),
+                    lineHeight = 16.69.sp
+                )
+            }
+        }
+        Spacer(modifier.height(15.dp))
+        TextField(
+            value = "",
+            onValueChange = {},
+            modifier
+                .fillMaxWidth(1f)
+                .clip(
+                    RoundedCornerShape(25.dp)
+                )
+        )
+        Spacer(modifier.height(40.dp))
+        Option(
+            heading = "Network & Internet",
+            desc = "Wi-Fi, Mobile, Data usage, Hotspot",
+            modifier = modifier
+        )
+        Spacer(modifier.height(41.dp))
+        Option(
+            heading = "Connected devices",
+            desc = "Bluetooth, Cast, NFC",
+            modifier = modifier
+        )
+        Spacer(modifier.height(41.dp))
+        Option(
+            heading = "App",
+            desc = "Permissions, default apps",
+            modifier = modifier
+        )
+        Spacer(modifier.height(41.dp))
+        Option(
+            heading = "Notifications",
+            desc = "Permissions, dafault apps",
+            modifier = modifier
+        )
+        Spacer(modifier.height(41.dp))
+        Option(
+            heading = "Digital wellbeing",
+            desc = "Screen time, app timer, bedtime schedules",
+            modifier = modifier
+        )
+
+    }
+}
+
+@Composable
+fun Option(heading: String, desc: String, modifier: Modifier) {
+    Row {
+        Box(
+            modifier
+                .width(47.dp)
+                .height(47.dp)
+                .clip(RoundedCornerShape(50))
+                .background(Color(0xFFE2E3D8))
+        )
+        Spacer(modifier.width(17.dp))
+        Column {
+            Text(text = heading, fontSize = 20.sp, fontWeight = FontWeight(500))
+            Spacer(modifier.height(4.dp))
+            Text(text = desc, fontSize = 14.sp, fontWeight = FontWeight(400))
         }
     }
 }
